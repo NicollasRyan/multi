8fb460e0c8a22ebede50c486852b9787bcc6786d
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-02-20 10:38:56 -0300

    criando e estilizando o FormStep1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,3 +1,33 @@
+import {
+  BoxText,
+  Button,
+  Conatiner,
+  Description,
+  Input,
+  Label,
+  Step,
+  Title,
+} from "./styles";
+
 export function FormStep1() {
-  return <h1>Form1</h1>;
+  const handleNextStep = () => {};
+
+  return (
+    <Conatiner>
+      <BoxText>
+        <Step>Passo 1/3</Step>
+        <Title>Vamos começar com seu nome</Title>
+        <Description>
+          Preencha o campo abaixo com seu nome completo.
+        </Description>
+      </BoxText>
+
+      <Label>
+        Seu Nome completo
+        <Input />
+      </Label>
+
+      <Button onClick={handleNextStep}>Próximo</Button>
+    </Conatiner>
+  );
 }
