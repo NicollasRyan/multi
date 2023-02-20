b46cb6ed5589b5b9721ea6e3da76550dec40bd89
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-02-20 12:44:53 -0300

    criando FormStep3

--- src/pages/FormStep3/index.tsx
+++ src/pages/FormStep3/index.tsx
@@ -1,3 +1,88 @@
+import { ChangeEvent, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm, FormAction } from "../../contexts/FormContext";
+
+import {
+  BackButton,
+  BoxText,
+  Button,
+  Conatiner,
+  Description,
+  Input,
+  Label,
+  Step,
+  Title,
+} from "./styles";
+
 export function FormStep3() {
-  return <h1>Form3</h1>;
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    if (state.name === "") {
+      navigate("/");
+    }
+    dispatch({
+      type: FormAction.setCurrentStep,
+      payload: 3,
+    });
+  }, []);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormAction.setEmail,
+      payload: e.target.value,
+    });
+  };
+
+  const handleGithubChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormAction.setGithub,
+      payload: e.target.value,
+    });
+  };
+
+  const handleNextStep = () => {
+    if (state.email !== "" && state.github !== "") {
+      navigate("/finished");
+      console.log(state);
+    } else {
+      alert("Preencha os dados.");
+    }
+  };
+
+  return (
+    <Conatiner>
+      <BoxText>
+        <Step>Passo 3/3</Step>
+        <Title>Proto {state.name}, onde te achamos?</Title>
+        <Description>
+          Preencha com seus contatos para conseguir entra em contato com voçê.
+        </Description>
+      </BoxText>
+
+      <Label>
+        Qual é seu email?
+        <Input
+          type="email"
+          autoFocus
+          value={state.email}
+          onChange={handleEmailChange}
+        />
+      </Label>
+
+      <Label>
+        Qual é seu Github?
+        <Input
+          type="url"
+          autoFocus
+          value={state.github}
+          onChange={handleGithubChange}
+        />
+      </Label>
+
+      <BackButton to="/step2">Voltar</BackButton>
+      <Button onClick={handleNextStep}>Pronto</Button>
+    </Conatiner>
+  );
 }
--- src/pages/FormStep3/styles.ts
+++ src/pages/FormStep3/styles.ts
@@ -1,1 +1,68 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+export const Conatiner = styled.div``;
+
+export const BoxText = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #16195c;
+  gap: 20px 10px;
+  padding-bottom: 25px;
+`;
+
+export const Step = styled.p`
+  font-size: 13px;
+  color: #b8b8d4;
+`;
+
+export const Title = styled.h1`
+  font-size: 26;
+`;
+
+export const Description = styled.div``;
+
+export const Label = styled.label`
+  display: block;
+  margin-top: 20px;
+  font-size: 13px;
+`;
+
+export const Input = styled.input`
+  margin-top: 7px;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 20px 10px;
+  border: 2px solid #25cd89;
+  border-radius: 10px;
+  color: #fff;
+  outline: 0;
+  font-size: 15px;
+  background-color: #02044a;
+`;
+
+export const Button = styled.button`
+  background-color: #25cd89;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 20px 40px;
+  border: 0;
+  border-radius: 30px;
+  cursor: pointer;
+  margin-top: 30px;
+`;
+
+export const BackButton = styled(Link)`
+  text-decoration: none;
+  background-color: #25cd89;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 20px 40px;
+  border: 0;
+  border-radius: 30px;
+  cursor: pointer;
+  margin-top: 30px;
+  margin-right: 20px;
+`;
